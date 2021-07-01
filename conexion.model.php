7635f27b5405b1b3aba271ece5bb8f0a9bdd3699
jonyDLC8 <67179299+jonyDLC8@users.noreply.github.com>
2021-07-01 14:29:30 -0600

    Update conexion.model.php

--- conexion.model.php
+++ conexion.model.php
@@ -1,1 +1,13 @@
-
+<?php
+    Class conexion extends Mysqli
+    {
+      function_construct()
+      {
+        $servidor="localhost";
+        $pass="";
+        $user="root";
+        $DB="Inventario5toDiver";
+        
+        parent::_construct($servidor,$user,$pass,$DB);
+        $this->query(SET NAMES 'utf8'; ");
+        $this->connect_errno ? die('Error en la conexion'): $error="conectado a ".$DB;
